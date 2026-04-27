adc8700bce131cad5afe2e178a824e9e50889d63
Kings-Israel <milimokings@gmail.com>
2026-04-27 12:11:17 +0300

    Updated APIs

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,8 @@
 import axios from "axios";
 
 export const http = axios.create({
-    baseURL: process.env.NODE_ENV == 'development' ? 'http://admin.farmside.test/api' : 'https://farmside.kingsisrael.com/api'
-})
+  baseURL:
+    process.env.NODE_ENV == "development"
+      ? "http://admin.farmside.test/api"
+      : "https://farmside-admin.kingsisrael.com/api",
+});
